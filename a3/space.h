5e551515a478e3d60946bd19325e9ca8d8d5e37e
Cotaroy <cotaroy.cheung@mail.utoronto.ca>
2026-03-16 15:22:51 -0400

    added new functions signatures for freeing memory

--- a3/space.h
+++ a3/space.h
@@ -17,5 +17,7 @@ Triangle *create_triangle(int *v1, int *v2, int *v3);
 Entity *create_entity(Triangle *object);
 // (x,y,z) is the coordinate of the front left bottom vertex of the rectangle
 Entity *create_rectangle(Entity *entities, int x, int y, int z, int x_length, int y_length, int z_length);
+void free_all_triangles(Triangle *triangles);
+void free_all_entities(Entity *entities);
 
 #endif
