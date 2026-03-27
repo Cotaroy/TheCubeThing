f34e27a65367890b82172fc4052f49f00a6771fe
Cotaroy <cotaroy.cheung@mail.utoronto.ca>
2026-03-26 20:13:12 -0400

    light now decreases quadratially i think

--- a3/renderer.c
+++ a3/renderer.c
@@ -2,20 +2,23 @@
 #include <stdio.h>
 #include <math.h>
 #include "renderer.h"
+#include "raycast.h"
 
-
+// there are 69 chars here not including space or null terminator
 static char *hierarchy = "$@B%8&WM#*oahkbdpqwmZO0QLCJUYXzcvunxrjft/\\|()1{}[]?-_+~<>i!lI;:,\"^`'."; // lightest to darkest
 
 char get_pixel_char(double distance) {
-    int num_tiers = 68;
-    double min_dist = 0.0;
-    double max_dist = 128.0;
-    double tier_step = (max_dist - min_dist) / num_tiers;
-
-    if(distance > max_dist) {
-        distance = max_dist;
+    if (distance < EPSILON) {
+        return hierarchy[0];
     }
-    int tier = (distance - min_dist) / tier_step;
+    if(distance > MAX_DIST) {
+        distance = MAX_DIST;
+    }
+    int num_tiers = 69;
+    
+    double relative_distance = (distance - MIN_DIST) / (MAX_DIST - MIN_DIST);
+
+    int tier = (int) (sqrt(relative_distance) * (num_tiers - 1));
 
     // printf("%f corresponds to character '%c', with tier: %d\n", distance, hierarchy[tier], tier);
     return hierarchy[tier];
--- a3/renderer.h
+++ a3/renderer.h
@@ -1,5 +1,8 @@
 #ifndef RENDERER_H
 #define RENDERER_H
+#define MAX_DIST 128.0
+#define MIN_DIST 0.0
+#define EPSILON 1e-8
 
 typedef struct {
     int width;
