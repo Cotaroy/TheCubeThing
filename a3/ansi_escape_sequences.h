2b9f4275c0d491849c2f89f7ae59d7d498fb3af9
Bill Huang <billrq.huang@mail.utoronto.ca>
2026-03-27 19:52:53 -0400

    Create ansi_escape_sequences.h

---
+++ a3/ansi_escape_sequences.h
@@ -0,0 +1,8 @@
+/**
+ * Escape sequences to manipulate the user's terminal display.
+ * 
+ * Data from
+ * - https://en.wikipedia.org/wiki/ANSI_escape_code
+ * - https://gist.github.com/ConnerWill/d4b6c776b509add763e17f9f113fd25b
+ */
+
