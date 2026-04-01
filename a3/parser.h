70f41e9955c53eb065378738ec0bcbb05348b808
Bill Huang <billrq.huang@mail.utoronto.ca>
2026-04-01 14:19:14 -0400

    added alias `q` for exit command

--- a3/parser.h
+++ a3/parser.h
@@ -1,7 +1,7 @@
 #ifndef PARSER_H
 #define PARSER_H
 
-#define NUM_AVAILABLE_COMMAND_HANDLERS 7
+#define NUM_AVAILABLE_COMMAND_HANDLERS 8
 
 // read from standard in and execture commands accordingly
 int parse_single_command();
